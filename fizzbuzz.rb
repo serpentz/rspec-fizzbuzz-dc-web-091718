e826b3b863921c80233b7c4ebdaa1e207c02e38f
Absalom Cherinet <absalomcherinet@gmail.com>
2018-08-27 17:24:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 def fizzbuzz(num)
 
 if(num%3== 0 && num%5)
-  puts "FizzBuzz"
+   "FizzBuzz"
 elsif num % 3 == 0
-  puts "Buzz"
+   "Buzz"
 elsif num % 5 == 0
-  puts "Fuzz"
+   "Fuzz"
 else
   return
 
